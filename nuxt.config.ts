8925c81035f512781dccc6cdae24511f20276a06
levch <levchenko.d.a1998@gmail.com>
2022-11-18 16:11:38 +0600

    initial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        {rel: 'manifest', href: '/manifest.json'}
+        {rel: 'manifest', href: '/manifest.json'},
+        {rel: 'icon', href: '/favicon.png'},
+      ],
+      meta: [
+        {name: 'viewport', content: 'width=device-width, user-scalable=no'},
       ],
     }
   }
